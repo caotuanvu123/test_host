ebd8e3f1dfec9a758979727b22dc5f67506d5534
VuTuan <vu.global.dev@gmail.com>
2024-07-30 14:23:43 +0700

    Auto commit at 2024-07-30 14:23:43

--- auto_commit.sh
+++ auto_commit.sh
@@ -2,16 +2,17 @@
 
 REPO_PATH="C:/Users/Administrator/Documents/price_project_visual_github"
 
-cd "$REPO_PATH" || { echo "Không thể thay đổi thư mục đến $REPO_PATH"; exit 1; }
+while true; do
+    cd "$REPO_PATH" || { echo "Không thể thay đổi thư mục đến $REPO_PATH"; exit 1; }
 
-NOW=$(date +"%Y-%m-%d %H:%M:%S")
-COMMIT_MESSAGE="Auto commit at $NOW"
+    NOW=$(date +"%Y-%m-%d %H:%M:%S")
+    COMMIT_MESSAGE="Auto commit at $NOW"
 
-git add .
-git commit -m "$COMMIT_MESSAGE"
-#git pull origin master
-git push origin master
+    git add .
+    git commit -m "$COMMIT_MESSAGE"
+    #git pull origin master
+    git push origin master
 
-# Đợi 10 phút (600 giây) trước khi thực hiện lần tiếp theo
-sleep 10
-    
+    # Đợi 10 phút (600 giây) trước khi thực hiện lần tiếp theo
+    sleep 10
+done
