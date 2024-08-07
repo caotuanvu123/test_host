badc0b261141c4e775bdbca9081660b6919108d8
caotuanvu198 <caotuanvu198@gmail.com>
2024-08-07 09:00:43 +0700

    Auto commit at 2024-08-07 09:00:43

--- auto_commit.sh
+++ auto_commit.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
+#sleep 40
 
 REPO_PATH="/home/yuyu/Documents/product_2024/ob-btc-future"
+cd "$REPO_PATH" || { echo "Không thể thay đổi thư mục đến $REPO_PATH"; exit 1; }
 
-while true; do
-    cd "$REPO_PATH" || { echo "Không thể thay đổi thư mục đến $REPO_PATH"; exit 1; }
-
-    NOW=$(date +"%Y-%m-%d %H:%M:%S")
-    COMMIT_MESSAGE="Auto commit at $NOW"
-    #aa
-    git add .
-    git commit -m "$COMMIT_MESSAGE"
-    #git pull origin master
-    git push origin master
-
-    # Đợi 10 phút (600 giây) trước khi thực hiện lần tiếp theo
-    sleep 620
-done
+NOW=$(date +"%Y-%m-%d %H:%M:%S")
+COMMIT_MESSAGE="Auto commit at $NOW"
+#aa
+git add .
+git commit -m "$COMMIT_MESSAGE"
+#git pull origin master
+git push origin master
